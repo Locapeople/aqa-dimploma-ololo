c43c98749cbd298f1f242829728faaaec71824f0
Locapeople <missis.anyack@yandex.ru>
2024-02-10 14:43:25 +0300

    update SUT stop script

--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # SUT stop script
 
+[[ ! -f "app.pid" ]] && { echo "Cannot stop SUT: no pid file (is it still running?)"; exit -1; }
 docker-compose down
 kill -9 $(cat app.pid) && rm -f app.pid
